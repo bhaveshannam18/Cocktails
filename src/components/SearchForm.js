4484e5c6deaf88444f645146abbfdf4750ceb0f6
Bhavesh Annam <bhaveshannam18@gmail.com>
2022-08-29 18:53:37 +0530

    api project

---
+++ src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext();
+  const searchValue = useRef("");
+  const searchCocktail = ()=>{
+    setSearchTerm(searchValue.current.value);
+  }
+  useEffect(()=>{
+    searchValue.current.focus();
+  },[])
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  }
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
